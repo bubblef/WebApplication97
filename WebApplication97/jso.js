3747298ccfba0a3ab4a90579d8ea1cf4ffde9347
Katya Kulikova <katya.kulikova@jetbrains.com>
2017-02-22 17:06:04 +0300

    Initial commit

---
+++ WebApplication97/jso.js
@@ -0,0 +1,10 @@
+
+function myFunction(a, b) {
+    return a * b;
+}
+
+
+function myFunction() {
+    var text = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = text.toUpperCase();
+}
